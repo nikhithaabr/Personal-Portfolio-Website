18c33787ce40feb25f390f17bfa508f81f4ea4cd
nikhithaabr <154864187+nikhithaabr@users.noreply.github.com>
2024-08-29 22:36:17 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import styles from "./App.module.css";
+import { About } from "./components/about/about";
+import { Contact } from "./components/contact/contact";
+import { Experience } from "./components/experience/experience";
+import { Hero } from "./components/hero/hero";
+import { Navbar } from "./components/navbar/navbar";
+import { Projects } from "./components/projects/Projects";
+
+function App() {
+  return (
+    <div style={{ backgroundColor: '#f0f0f0', height: '100vh' }}>
+    </div>
+  )
+}
+
+export default App;
